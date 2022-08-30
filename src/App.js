a0e59917df29439c6344f1f6037447c1d72e3739
Vladimir Montiel <vladiwar1@gmail.com>
2022-08-30 12:57:39 -0500

    Primer version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, {useState, useEffect } from 'react';
 import './App.css';
+//import { format } from 'da';
 
 function App() {
+  const [atmosfericas, setAtmosfericas] = useState(null);
+  const [valor, setValor] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.datos.gob.mx/v1/condiciones-atmosfericas').then(res =>{
+      return res.json()
+    }).then(res =>{
+      //console.log('Respuesta...', res.results);
+      setAtmosfericas(res.results);
+    })
+  })
+  const formato = (value) =>{
+    const fecha = value;
+    return fecha;
+  };
+
+  const idEspecifico = () =>{
+    
+  };
+  //console.log(atmosfericas);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="table">
+        <thead className='filaHead'>
+          <tr>
+            <th>_id</th>
+            <th>cityId</th>
+            <th>name</th>
+            <th>state</th>
+            <th>probability</th>
+            <th>relativehu</th>
+            <th>Lastreporttime</th>
+            <th>LLUEVE</th>
+          </tr>
+        </thead>
+        <tbody className='filaBody'>
+          {atmosfericas &&
+            atmosfericas.slice(0,10).map((clima) =>  {
+              return (
+                <tr key={clima.id}>
+                  <td onClick={idEspecifico}> <a>{clima.id}</a> </td>
+                  <td>{clima.cityid}</td>
+                  <td>{clima.name}</td>
+                  <td>{clima.state}</td>
+                  <td>{clima.probabilityofprecip}</td>
+                  <td>{clima.relativehumidity}</td>
+                  <td>{formato(clima.lastreporttime) }</td>
+                  <td>{ clima.relativehumidity >50 ||  clima.probabilityofprecip >60 ? 'Llueve' : 'No llueve'  }</td>
+                </tr>
+              );
+              
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }
